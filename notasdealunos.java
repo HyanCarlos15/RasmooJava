963393d66d319c32532b8aa171d3a838b2a942fd
Hyan Carlos Rodrigues da Silva <hyansilva177@gmail.com>
2024-08-13 21:33:46 -0300

    Media entre notas de Alunos
    Realizando uma atribuição de Variavel em Numero flutuante (Double) e realizando uma soma total das notas, uma media de classe entre 4 alunos e a realização de uma expressão simples utilizando as variaveis media e soma

---
+++ notasdealunos.java
@@ -0,0 +1,24 @@
+public class notasdealunos {
+    public static void main (String[] args){
+
+        double aluno1 = 7.4;
+        double aluno2 = 8.9;
+        double aluno3 = 6.0;
+        double aluno4 = 5.3;
+
+        double soma = (aluno1 + aluno2 + aluno3 + aluno4);
+
+        double media = (soma/4);
+
+        double expressão = (soma + media * 4 + 5/3);
+
+        System.out.println("A media da classe na sala é de; " + media);
+
+
+        System.out.println("A soma das notas é " + soma);
+
+        System.out.println("O Resultado da Expressão é: " + expressão);
+
+    }
+
+}
