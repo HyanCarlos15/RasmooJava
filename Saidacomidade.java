1237efb92fc1a62406ca2078203b0c69c76b9863
Hyan Carlos Rodrigues da Silva <hyansilva177@gmail.com>
2024-08-13 21:31:30 -0300

    Add files via upload

---
+++ Saidacomidade.java
@@ -0,0 +1,16 @@
+public class Saidacomidade{
+     public static void main(String[] args) {
+
+         int idadeHyan = 24;
+         int idadeMilene = 20;
+
+         int somadasidades = (idadeMilene + idadeHyan);
+
+         System.out.println("A idade de Milene é: " + idadeMilene);
+         System.out.println("A idade de Hyan é: " + idadeHyan);
+
+         System.out.println(" A soma das idades é: " + somadasidades);
+
+     }
+
+}
